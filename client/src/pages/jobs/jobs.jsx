5a9c05c951b9e2f0e8ff5b4014f7a7df8b0e74f6
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-30 16:35:34 +0300

    jobs

---
+++ client/src/pages/jobs/jobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateJobs from './createJobs'
+import JobList from './jobsList'
+
+function Jobs() {
+  return (
+    <div>
+      <CreateJobs/>
+      <JobList/>
+    </div>
+  )
+}
+
+export default Jobs
