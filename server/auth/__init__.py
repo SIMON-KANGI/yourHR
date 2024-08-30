9e3a1486ac67767bd692eee6d5c88495e5f0db3d
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-08-30 16:37:59 +0300

    auth

--- server/auth/__init__.py
+++ server/auth/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from config import create_app
 from flask_restful import Api
-from .routes import Login, UserToken
+from .routes import Login, UserToken,Logout
 app = create_app()
 api = Api(app)
 
@@ -10,6 +10,7 @@ api = Api(auth_blueprint)
 
 api.add_resource(Login, '/login')
 api.add_resource(UserToken, '/token')
+api.add_resource(Logout, '/logout')
 
 
 __all__ = ['user_blueprint']
