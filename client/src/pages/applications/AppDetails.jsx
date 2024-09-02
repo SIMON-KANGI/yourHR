4e87d83d05e46766b748a4774b9e672b1b2dfb05
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-02 12:10:35 +0300

    applicant details

--- client/src/pages/applications/AppDetails.jsx
+++ client/src/pages/applications/AppDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
+import { FaCloudDownloadAlt } from "react-icons/fa";
 function AppDetails() {
     const location= useLocation()
     const {state}= location;
@@ -9,9 +10,29 @@ function AppDetails() {
       <h1 className='text-center text-2xl font-bold'>Applicant Details</h1>
       <section className='w-3/4 mx-auto shadow-lg p-4'>
         <img src={applicant.user?.profile} alt={applicant.user.username} className='w-28 h-28 rounded-full' />
-      <h1 className='font-bold text-xl'>Name: {applicant.user?.username}</h1>
-      <h1 className='font-bold text-xl'>Email: {applicant.user?.email}</h1>
-      <h1 className='font-bold text-xl'>Role: {applicant.user?.role}</h1>
+        <div className='my-4 shadow-md p-4'>
+           <h1 className='text-xl'>Name: <span className="font-bold">{applicant.user?.username}</span></h1>
+      <h1 className='text-xl'>Email: <span className='font-bold'>{applicant.user?.email}</span></h1>
+      <h1 className='text-xl'>Role: <span className='font-bold'>{applicant.user?.skill}</span></h1>  
+        </div>
+     
+      <div className='border border-gray-600 p-4'>
+      <h1 className='text-center font-bold text-xl'>Cover Letter</h1>
+        <p>
+            {applicant.letter}
+        </p>
+      </div>
+      <button 
+
+      className="flex px-8 py-3 my-4 text-white items-center bg-gradient-to-r from-blue-600 to-teal-500 rounded-md text-center">
+      <a href={applicant.resume} className='flex items-center'>
+         <FaCloudDownloadAlt size="28px" />  
+      Download Resume
+      </a>
+     
+       
+       
+      </button>
       </section>
       
     </div>
