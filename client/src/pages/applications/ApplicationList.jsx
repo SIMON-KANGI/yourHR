faa1ab514ca42e11f5675a7973b5aed414d3c595
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-02 10:05:23 +0300

    applications list

--- client/src/pages/applications/ApplicationList.jsx
+++ client/src/pages/applications/ApplicationList.jsx
@@ -1,54 +1,44 @@
+import { Tooltip } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
-
+import { useNavigate } from 'react-router-dom';
+import { FaEye } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 function ApplicationList({ applications }) {
-  const [applicantDetails, setApplicantDetails] = useState([]);
-
-  const fetchUsers = async () => {
-    const res = await axios.get('https://yourhr-2des.onrender.com/user/users');
-    return res.data;
-  };
-
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['users'],
-    queryFn: fetchUsers,
-  });
-
-  useEffect(() => {
-    if (data) {
-      // Filter users based on the application.user_id
-      const filteredApplicants = data.filter(user =>
-        applications.some(app => app.user_id === user.id)
-      );
-      setApplicantDetails(filteredApplicants);
-    }
-  }, [data, applications]);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-if(applications.length <1){
-    return <div>No applications yet.</div>
-  
-}
+ const navigate=useNavigate()
   return (
     <div>
       {applications.length > 0 && (
         <div>
-          <h1 className='text-xl font-bold'>Applications</h1>
-          <table>
+          <h1 className='text-xl font-bold text-center mb-6'>Applications</h1>
+          <table className='min-w-full table-auto'>
 <thead>
     <tr>
-      <th>Name</th>
-      <th>Email</th>
+      <th className='px-4 py-2'>Name</th>
+      <th className='px-4 py-2'>Email</th>
+      <th className='px-4 py-2'>Status</th>
+      <th  className='px-4 py-2'>Actions</th>
     </tr>
   
 </thead>
-            {applicantDetails.map((applicant, index) => (
-              <tr key={index} className='my-2'>
-                <td>{applicant.username}</td>
-                <td>{applicant.email}</td>
-
+            {applications.map((applicant, index) => (
+              <tr key={index} className=' border-b items-center'>
+                <td  className='px-4 py-2 text-center'>{applicant.user.username}</td>
+                <td  className='px-4 py-2 text-center'>{applicant.user.email}</td>
+                <td  className='px-4 py-2 text-center'>
+                <button className='bg-rose-600 text-gray-200 py-2 px-6 rounded-lg'>
+                     {applicant.status}
+                </button>
+             
+                </td>
+                <td className='px-4 py-2 text-center'>  
+                 <Tooltip hasArrow label="View more">
+                  <button
+                   onClick={()=>navigate(`/jobs/applicants/${applicant.user.username}`)}
+                   className='bg-transparent  hover:text-green-600 py-2 px-6 rounded-lg'>
+                <FaEye size="28px"/>
+                </button>
+                   </Tooltip>
+                </td>
               </tr>
             ))}
           </table>
