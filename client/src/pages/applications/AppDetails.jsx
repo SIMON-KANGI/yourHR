eae77b306b86418c7b2d87dda2e37e845830926f
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-02 11:41:00 +0300

    applicant details

--- client/src/pages/applications/AppDetails.jsx
+++ client/src/pages/applications/AppDetails.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import { useLocation } from 'react-router-dom'
 function AppDetails() {
+    const location= useLocation()
+    const {state}= location;
+    const applicant= state?.applicant
   return (
     <div>
+      <h1 className='text-center text-2xl font-bold'>Applicant Details</h1>
+      <section className='w-3/4 mx-auto shadow-lg p-4'>
+        <img src={applicant.user?.profile} alt={applicant.user.username} className='w-28 h-28 rounded-full' />
+      <h1 className='font-bold text-xl'>Name: {applicant.user?.username}</h1>
+      <h1 className='font-bold text-xl'>Email: {applicant.user?.email}</h1>
+      <h1 className='font-bold text-xl'>Role: {applicant.user?.role}</h1>
+      </section>
       
     </div>
   )
