0a80e781ca4eca8dcfb580592ef7091d73894450
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-03 08:57:32 +0300

    app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/auth/login'
 import Jobs from './pages/jobs/jobs'
 import JobDetails from './pages/jobs/jobDetails'
 import Home from './pages/Home'
+import AppDetails from './pages/applications/AppDetails'
 function App() {
   return (
     <main id="app" className='min-h-screen'>
@@ -16,6 +17,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/jobs" element={<Jobs />} />
       <Route path="/jobs/:title" element={<JobDetails />} />
+      <Route path="/jobs/applicants/:username" element={<AppDetails />} />
     </Routes>
     </main>
   )
