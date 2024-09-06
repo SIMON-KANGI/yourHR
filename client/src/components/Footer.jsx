125688d4d6de8d7e2481aebfebdee98995867fe0
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-06 13:36:26 +0300

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 
 import { FaGithub } from "react-icons/fa";
-
-
+import { FaLinkedin } from "react-icons/fa";
+import { TbWorld } from "react-icons/tb";
+import { BsTwitterX } from "react-icons/bs";
+import { Link, useNavigate } from "react-router-dom";
 function Footer(){
+    const navigate=useNavigate()
     return(
         <div className="w-3/4 mx-auto bg-black p-16 my-8 rounded-md">
 <section className="flex justify-around">
@@ -54,17 +57,32 @@ function Footer(){
         <li>Modern slavery statement</li>
     </ul>
 </section>
-<section className="my-4">
+<section className="my-4 flex items-center">
     <h1 className="text-xl text-stone-200 font-bold">Follow me</h1>
-<div>
-    <button className="w-12 h-12 justify-center rounded-full border border-stone-200 items-center">
-<FaGithub color="white" size="24" />
+<div className="flex">
+    <button className="w-12 h-12 mx-2 justify-center flex rounded-full border border-stone-200 items-center">
+    <Link to="https://github.com/SIMON-KANGI">
+        <FaGithub color="white" size="24" />
+    </Link>
+
     </button>
-    <button className="w-12 h-12 justify-center rounded-full border border-stone-200 items-center">
-<FaGithub color="white" size="24" />
+    <button className="w-12 h-12 justify-center flex rounded-full border border-stone-200 items-center">
+    <Link to="https://www.linkedin.com/in/simon-mwangi-b22925200/">
+        <FaLinkedin color="white" size="24" />
+    </Link>
+
     </button>
-    <button className="w-12 h-12 justify-center rounded-full border border-stone-200 items-center">
-<FaGithub color="white" size="24" />
+    <button className="w-12 mx-2 h-12 flex justify-center rounded-full border border-stone-200 items-center">
+    <Link to="https://portfolio-mu-five-65.vercel.app/">
+        <TbWorld color="white" size="24" />   
+    </Link>
+ 
+    </button>
+    <button className="w-12 mx-2 h-12 flex justify-center rounded-full border border-stone-200 items-center">
+    <Link to="https://x.com/Kangisimo">
+       <BsTwitterX color="white" size="24" />    
+    </Link>
+ 
     </button>
 </div>
 </section>
