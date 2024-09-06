8bd548664128a0a79aee41400748b349dd7b1a07
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-06 10:21:59 +0300

    navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -24,12 +24,12 @@ function NavBar() {
        </Link></li>
        {!isAuthenticated &&(
         <div className='flex'>
-           <li className='mx-4 hover:font-bold bg-black text-white rounded-full py-2 px-6'>
+           <li className='mx-4 hover:font-bold bg-transparent text-black  py-2 px-6'>
            <Link to="/login">
            Login
            </Link>
            </li>
-       <li className=' hover:font-bold bg-gradient-to-r from-green-700 to-teal-600 text-white rounded-full py-2 px-6'>
+       <li className=' hover:font-bold bg-gradient-to-r from-green-700 to-teal-600 text-white rounded-lg py-2 px-6'>
        <Link to="/signup">Register</Link>
        </li>
       
