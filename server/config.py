5874c27edfae4abb318e53bbe7b6c3a58f84d41c
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-09-19 08:06:57 +0300

    GOOGLE LOGIN

--- server/config.py
+++ server/config.py
@@ -34,7 +34,9 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///hr.db')
     app.config['JWT_SECRET_KEY'] = secret_key
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
+    app.config['GOOGLE_CLIENT_ID'] = os.getenv('GOOGLE_CLIENT_ID')
+    app.config['GOOGLE_CLIENT_SECRET'] = os.getenv('GOOGLE_CLIENT_SECRET')
+    app.config['GOOGLE_DISCOVERY_URL'] = ("https://accounts.google.com/.well-known/openid-configuration")
     jwt = JWTManager(app)
     db.init_app(app)
     migrate.init_app(app, db)
